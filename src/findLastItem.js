103a8678e42cd59d32210b0215c395565b51365a
Banb4n <alban.such@gmail.com>
2018-10-09 00:31:11 +0200

    Finish findLastItem and add more tests

--- src/findLastItem.js
+++ src/findLastItem.js
@@ -1,3 +1,30 @@
-export const findLastItem = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+export const findLastItem = (value) => {
+  if (Array.isArray(value)) {
+    return lastInArray(value);
+  } else if (value instanceof Object) {
+    return lastInObject(value);
+  } else if (typeof value === 'string') {
+    return lastInString(value);
+  }
+};
+
+const lastInString = (str) => str.slice(str.length - 1, str.length);
+
+const lastInArray = (arr) => {
+  const lastItem = arr.pop();
+
+  if (Array.isArray(lastItem)) {
+    return lastInArray(lastItem);
+  } else if (lastItem instanceof Object && !Array.isArray(lastItem)) {
+    return lastInObject(lastItem);
+  } 
+
+  return lastItem;
+};
+
+const lastInObject = (obj) => {
+  const lastKeys = lastInArray(Object.keys(obj));
+  const lastItem = lastInArray(Object.values(obj));
+
+  return { [lastKeys]: lastItem };
 };
--- src/findLastItem.test.js
+++ src/findLastItem.test.js
@@ -1,18 +1,27 @@
 import { findLastItem } from './findLastItem';
 
 const TESTS = [
+  // Simple Array 
   {
     value: [1, 2, 3, 4],
     expected: 4
   },
+  // Simple String
   {
     value: 'hello',
-    expected: 'h'
+    expected: 'o'
   },
+  // Simple Object
   {
     value: { a: 1, b: 2, c: 3 },
     expected: { c: 3 }
   },
+  // Recursive Array
+  {
+    value: [1, 2, 3, ['a', 'b', 'c']],
+    expected: 'c'
+  },
+  // Recursive Object
   {
     value: { a: false, b: 'value', c: { a1: 'a', a2: 'b' } },
     expected: { c: { a2: 'b'} }
@@ -20,9 +29,23 @@ const TESTS = [
 ]
 
 describe('findLastItem', () => {
-  TESTS.forEach(test => {
-    it(`${test.value} => ${test.expected}`, () => {
-      expect(findLastItem(test.value)).toBe(test.expected);
-    });
-  })
+  it(`[1, 2, 3, 4] ===> 4`, () => {
+    expect(findLastItem([1, 2, 3, 4])).toBe(4);
+  });
+
+  it('hello ===> o', () => {
+    expect(findLastItem('hello')).toBe('o');
+  });
+
+  it(`{ a: 1, b: 2, c: 3 } ===> { c: 3 }`, () => {
+    expect(findLastItem(test.value)).toEqual(test.expected);
+  });
+
+  it(`[1, 2, 3, ['a', 'b', 'c']] ===> 'c'`, () => {
+    expect(findLastItem([1, 2, 3, ['a', 'b', 'c']])).toBe('c');
+  });
+
+  it(`{ a: false, b: 'value', c: { a1: 'a', a2: 'b' } } ===> { c: { a2: 'b'} }`, () => {
+    expect(findLastItem({ a: false, b: 'value', c: { a1: 'a', a2: 'b' } })).toEqual({ c: { a2: 'b'} });
+  });
 })
